da85858df61ad5ac75b9dd2425ec2909b741f3d7
bruno.pereira.silva <bruno.pereira.silva@accenture.com>
2023-03-29 17:47:18 -0300

    Create Initial BackEnd

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 Projeto de Desafio Full Stack para alocação
 
 Tecnologias Usadas:
+
+BACK_END:
 ->Spring Boot
+->maven
+->JDK 17(LTS)
 ->Banco MySQL
+
+FRONT_END:
 ->Angular 13
 ->VsCode
+->SCSS
+->Angular Material
+->HTML
---
+++ back-end-spring-boot/crud-spring-backEnd/crud-spring-backend/src/test/java/acc/br/crudspringbackend/CrudSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.br.crudspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
